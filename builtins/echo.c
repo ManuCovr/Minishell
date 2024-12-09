2f7def4b046765853d6e2185c6e5121bac684c5f
ManuCovr <mtmpfb@gmail.com>
2024-12-09 11:22:20 +0000

    tests

---
+++ builtins/echo.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-maga <mtmpfb@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 12:54:21 by mde-maga          #+#    #+#             */
+/*   Updated: 2024/12/09 11:15:59 by mde-maga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
+
+# define ERROR 1
+# define SUCCESS 0
+
+static void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, strlen(s));
+}
+
+static int	ft_strcmp(char *s1, char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s1)
+		return (1);
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+static int	nb_args(char **args)
+{
+	int		size;
+
+	size = 0;
+	while (args[size])
+		size++;
+	return (size);
+}
+
+
+int	ft_echo(char **args)
+{
+	int		i;
+	int		n_option;
+
+	i = 1;
+	n_option = 0;
+	if (nb_args(args) > 1)
+	{
+		while (args[i] && ft_strcmp(args[i], "-n") == 0)
+		{
+			n_option = 1;
+			i++;
+		}
+		while (args[i])
+		{
+			ft_putstr_fd(args[i], 1);
+			if (args[i + 1] && args[i][0] != '\0')
+				write(1, " ", 1);
+			i++;
+		}
+	}
+	if (n_option == 0)
+		write(1, "\n", 1);
+	return (SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+    ft_echo(argv + 1);
+    return 0;
+}
+
