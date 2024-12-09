680534bc6fc885d3d6ed0f610404477a7ea30804
ManuCovr <mtmpfb@gmail.com>
2024-12-09 18:08:04 +0000

    mamma mia

--- execute/builtins/cd.c
+++ execute/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: mde-maga <mtmpfb@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:27:20 by mde-maga          #+#    #+#             */
-/*   Updated: 2024/12/09 17:41:43 by mde-maga         ###   ########.fr       */
+/*   Updated: 2024/12/09 18:05:48 by mde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,29 @@ static void cd_error_manager(void)
 	ft_putstr_fd("Cannot access parent directories: ", 2);
 	ft_putstr_fd("No such file or directory\n", 2);
 	exit_status(1);
+}
+
+static char *message(void)
+{
+	char *path;
+
+	path = get_env("HOME"); // dont forget to create get_env
+	if(!path)
+	{
+		ft_putstr_fd("bash: cd: HOME not set\n", 2);
+		exit_status(1); // make exit_status
+		return NULL;
+	}
+	return (path);
+}
+
+void change_directory(char **cmd)
+{
+	char s[PATH_MAX];
+	char new[PATH_MAX];
+	char *path;
+	char *old_pwd;
+
+	if(!cmd[1])
+		
 }
--- headers/minishell.h
+++ headers/minishell.h
@@ -6,24 +6,32 @@
 /*   By: mde-maga <mtmpfb@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:18:11 by mde-maga          #+#    #+#             */
-/*   Updated: 2024/12/09 17:43:34 by mde-maga         ###   ########.fr       */
+/*   Updated: 2024/12/09 18:02:53 by mde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+#ifndef _POSIX_C_SOURCE 200112L
+# define _POSIX_C_SOURCE 200112L
+
 # include "../../libft/libft.h"
-# include <stdlib.h>
-# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 # include <stdio.h>
-# include <string.h>
-# include <fcntl.h>
-# include <dirent.h>
-# include <sys/wait.h>
+# include <unistd.h>
+# include <signal.h>
 # include <limits.h>
 # include <errno.h>
-# include <signal.h>
+# include <sys/wait.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <string.h>
+# include <stdbool.h>
+# include <dirent.h>
+# include <fcntl.h>
 
 # define ERROR 1
 # define SUCCESS 0
@@ -31,4 +39,5 @@
 
 int	ft_echo(char **args);
 
+#endif
 #endif
